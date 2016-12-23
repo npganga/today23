3275b60f3a87c9db833afffae5ad3c19f9e9d8ee
npganga <gangangpalayam@gmail.com>
2016-12-23 10:05:44 +0530

    Add files via upload

---
+++ sumnloop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,sum=0;
+printf("\n enter the number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+sum=sum+i;
+printf("\n the sum of n number is %d",sum);
+
+}
